b58510329b65816252c0865354e06cffaeb91633
celif <cfeli031@fiu.edu>
2019-08-31 15:19:31 -0400

    Access sections by clicking navbar links

--- carlos-feli-portfolio/Pages/Index.cshtml.cs
+++ carlos-feli-portfolio/Pages/Index.cshtml.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace carlos_feli_portfolio.Pages
+namespace about_carlos_feliciano.Pages
 {
     public class IndexModel : PageModel
     {

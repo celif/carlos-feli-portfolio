84554a0a4a79b5de96cb5eeab54e9a1801984cab
celif <cfeli031@fiu.edu>
2019-09-01 02:05:15 -0400

    Setup Selenium to automatically test navbar functionality

--- carlos-feli-portfolio/SeleniumTest.cs
+++ carlos-feli-portfolio/SeleniumTest.cs
@@ -23,7 +23,39 @@ namespace SeleniumTest
         [Test]
         public void test()
         {
-            driver.Url = "http://www.google.co.in";
+            driver.Url = "https://carlos-feli-portfolio-dev-as.azurewebsites.net/";
+
+            // Have Selenium automatically click the links mentioned below to ensure that the
+            // the user is taken to the correct section of the page.
+
+            IWebElement homepageLink = driver.FindElement(By.Id("carlos-feliciano"));
+            homepageLink.Click();
+            String URL = driver.Url;
+            Assert.AreEqual(URL, "https://carlos-feli-portfolio-dev-as.azurewebsites.net/#home");
+
+            // Test clicking the about link
+            IWebElement aboutLink = driver.FindElement(By.Id("about-link"));
+            aboutLink.Click();
+            URL = driver.Url;
+            Assert.AreEqual(URL, "https://carlos-feli-portfolio-dev-as.azurewebsites.net/#home");
+
+            // Test clicking the skills link
+            IWebElement skillsLink = driver.FindElement(By.Id("skills-link"));
+            skillsLink.Click();
+            URL = driver.Url;
+            Assert.AreEqual(URL, "https://carlos-feli-portfolio-dev-as.azurewebsites.net/#skills");
+
+            // Test clicking the projects link
+            IWebElement projectsLink = driver.FindElement(By.Id("projects-link"));
+            projectsLink.Click();
+            URL = driver.Url;
+            Assert.AreEqual(URL, "https://carlos-feli-portfolio-dev-as.azurewebsites.net/#projects");
+
+            // Test clicking the experience link
+            IWebElement experienceLink = driver.FindElement(By.Id("experience-link"));
+            experienceLink.Click();
+            URL = driver.Url;
+            Assert.AreEqual(URL, "https://carlos-feli-portfolio-dev-as.azurewebsites.net/#experience");
         }
 
         [TearDown]

85c76efb76bf27740282a5f468dc810e22902f01
celif <cfeli031@fiu.edu>
2019-09-01 13:03:18 -0400

    Added wwwroot to csproj and startup

--- carlos-feli-portfolio/Startup.cs
+++ carlos-feli-portfolio/Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -9,6 +10,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 
 namespace carlos_feli_portfolio
 {
@@ -50,6 +52,13 @@ namespace carlos_feli_portfolio
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+                Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot"))
+,
+                RequestPath = new PathString("/wwwroot")
+            });
             app.UseCookiePolicy();
 
             app.UseMvc();
